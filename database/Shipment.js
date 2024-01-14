0b9fc9411c39be1f2ea9da2eb5b28566a46d3dcb
NhanCSE <tranthenhan.cse@gmail.com>
2024-01-14 12:34:20 +0700

    "add try catch"

--- database/Shipment.js
+++ database/Shipment.js
@@ -16,33 +16,39 @@ const table = "shipment";
 const pool = mysql.createPool(dbOptions).promise();
 
 const getDataForShipmentCode = async (staff_id, transport_partner_id = null) => {
-    const staffTable = "staff";
-    const transportPartnerTable = "transport_partner";
-    const vehicleTable = "vehicle";
+    try {
+        const staffTable = "staff";
+        const transportPartnerTable = "transport_partner";
+        const vehicleTable = "vehicle";
+        
     
-
-    if(transport_partner_id !== null) {
-        const transportPartnerQuery = `SELECT name FROM ${transportPartnerTable} WHERE transport_partner_id = ?`;
-        const vehicleQuery = `SELECT vehicle_id FROM ${vehicleTable} WHERE transport_partner_id = ?`;
-        const [transportPartnerResult] = await pool.query(transportPartnerQuery, transport_partner_id);
-        const [vehicleResult] = await pool.query(vehicleQuery, transport_partner_id);
-        const result = {
-            partnerName: transportPartnerResult[0].name,
-            VehicleID: vehicleResult[0].vehicle_id,
+        if(transport_partner_id !== null) {
+            const transportPartnerQuery = `SELECT name FROM ${transportPartnerTable} WHERE transport_partner_id = ?`;
+            const vehicleQuery = `SELECT vehicle_id FROM ${vehicleTable} WHERE transport_partner_id = ?`;
+            const [transportPartnerResult] = await pool.query(transportPartnerQuery, transport_partner_id);
+            const [vehicleResult] = await pool.query(vehicleQuery, transport_partner_id);
+            const result = {
+                partnerName: transportPartnerResult[0].name,
+                VehicleID: vehicleResult[0].vehicle_id,
+            }
+            return result;
         }
-        return result;
-    }
-    else {
-        const staffQuery = `SELECT fullname FROM ${staffTable} WHERE staff_id = ?`;
-        const transportPartnerQuery = `SELECT vehicle_id FROM ${vehicleTable} WHERE staff_id = ?`;
-        const [staffResult] = await pool.query(staffQuery, staff_id);
-        const [transportPartnerResult] = await pool.query(transportPartnerQuery, staff_id);
-        const result = {
-            fullname: staffResult[0].fullname,
-            VehicleID: transportPartnerResult[0].vehicle_id,
+        else {
+            const staffQuery = `SELECT fullname FROM ${staffTable} WHERE staff_id = ?`;
+            const transportPartnerQuery = `SELECT vehicle_id FROM ${vehicleTable} WHERE staff_id = ?`;
+            const [staffResult] = await pool.query(staffQuery, staff_id);
+            const [transportPartnerResult] = await pool.query(transportPartnerQuery, staff_id);
+            const result = {
+                fullname: staffResult[0].fullname,
+                VehicleID: transportPartnerResult[0].vehicle_id,
+            }
+            return result;
         }
-        return result;
-    }
+    } catch (error) {
+        console.log("Error: ", error);
+        throw error;
+    } 
+    
 }
 
 const createNewShipment = async (fields, values) => {
